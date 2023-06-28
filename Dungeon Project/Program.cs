29a193bab942133f6d7e9bf05f5a171f973d593f
Alex Davenport <alexd8052@gmail.com>
2023-06-27 20:32:04 -0500

    added classes

--- Dungeon Project/Program.cs
+++ Dungeon Project/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Dungeon_Project
+﻿using DungeonLibrary;
+using System.Numerics;
+
+namespace Dungeon_Project
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-        }
-    }
-}
+            #region Introduction
+            //TODO Intro
+            Console.WriteLine("Hello Adventurer!");
+            #endregion
+
+            #region Player Creation
+
+            #endregion
+
+            bool quit = false;
+            do
+            {
+                #region Monster and room generation
+                
+                #endregion
+
+                #region Encounter Loop                
+                
+                #endregion
+            } while (!quit);
+
+            #region Exit
+            //TODO Exit
+            #endregion
+        }//End Main()
+
+
+    }//End class
+}//End namespace
